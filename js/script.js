4efa86b8f349f55464a6b37d60c82c99a23ec02b
LauraArredondoDev <lauraarredondodev@gmail.com>
2025-09-29 00:38:50 +0200

    validaciones de formulario y envio

--- js/script.js
+++ js/script.js
@@ -1,43 +1,166 @@
 function enviarMail() {
-    const inputNombre = document.getElementById('input-nombre');
-    const inputApellidos = document.getElementById('input-apellidos');
-    const inputTelefono = document.getElementById('input-telefono');
-    const inputCorreo = document.getElementById('input-correo');
-    const inputTextArea = document.getElementById('input-mensaje');
-    const form = document.getElementById('formulario');
 
-    const nombre = inputNombre.value;
-    const apellidos = inputApellidos.value;
-    const telefono = inputTelefono.value;
-    const correo = inputCorreo.value;
-    const textArea = inputTextArea.value;
-    const saltoLineaGmail = '%0D%0A';
-    const mensaje = textArea + saltoLineaGmail + 'Un saludo' + saltoLineaGmail + nombre + ' ' + apellidos + ', ' + telefono + ', ' + correo;
+    const listaInputs = document.querySelectorAll('form input, form textarea');
 
-    
-    
+    let deboEnviarMail = true;
 
-    // lista con los elementos del formulario
-    let campos = form.querySelectorAll('input, textarea')
-    let valido = true;
+    for(const input of listaInputs) {
 
-    //Recorremos los campos para comprobar si están vacios
-    for (let i = 0; i < campos.length; i++) {
-        let campo = campos[i];
-
-        if(campo.value.trim() === ' ') { //Si campo está vacío se pone el borde rojo y el formulario no es válido
-            campo.style.border = '2px solid red';
-            valido = false;
-
-        }else { //Si campo está completo se pone el borde verde y se abre la ventana de gmail
-            campo.style.border = '2px solid green';
-            window.open(urlGmail, '_blank');
+        if(comprobarCampoCorrecto(input.id) == false) {
+            deboEnviarMail = false;
         }
     }
 
-    const emailClinica = 'lauraarredondodev@gmail.com';
-    const urlGmail = 'https://mail.google.com/mail/?view=cm&fs=1&to=' + emailClinica + '&su=Cita&body=' + mensaje;
+    if(deboEnviarMail) {
+        //hacer cosas
+    }
+    
+}
 
-    // window.open(urlGmail, '_blank');
+function comprobarCampoCorrecto(idCampo) {
 
+    const esVacio = comprobarCampoVacio(idCampo);
+
+    const campo = document.getElementById(idCampo);
+    let caracteresCorrectos = false;
+
+    if(campo.value.trim() !== '') {
+
+        caracteresCorrectos = comprobarCaracteresCorrectos(idCampo);
+
+    }
+
+    const todoCorrecto = !esVacio && caracteresCorrectos;
+
+    return todoCorrecto;
+}
+
+function comprobarCampoVacio(idCampo) {
+
+    const campo = document.getElementById(idCampo);
+
+    const spanVacioNombre = document.getElementById('error-vacio-input-nombre');
+    const spanVacioApellidos = document.getElementById('error-vacio-input-apellidos');
+    const spanVacioTelefono = document.getElementById('error-vacio-input-telefono');
+    const spanVacioCorreo = document.getElementById('error-vacio-input-correo');
+    const spanVacioMensaje = document.getElementById('error-vacio-input-mensaje');
+
+    let campoVacio = campo.value.trim() === '';
+
+    if(!campoVacio) { //si no esta vacio
+        
+        campo.style.borderColor='#dee2e6'
+
+        switch(idCampo) {
+            case 'input-nombre':
+                spanVacioNombre.style.display='none';
+                break;
+            case 'input-apellidos':
+                spanVacioApellidos.style.display='none';
+                break;
+            case 'input-telefono':
+                spanVacioTelefono.style.display='none';
+                break;
+            case 'input-correo':
+                spanVacioCorreo.style.display='none';
+                break;
+            case 'input-mensaje':
+                spanVacioMensaje.style.display='none';
+                break;
+            default:
+                break;
+        }
+
+    } else {
+
+        campo.style.borderColor='red';
+
+        switch(idCampo) {
+            case 'input-nombre':
+                spanVacioNombre.style.display='block';
+                break;
+            case 'input-apellidos':
+                spanVacioApellidos.style.display='block';
+                break;
+            case 'input-telefono':
+                spanVacioTelefono.style.display='block';
+                break;
+            case 'input-correo':
+                spanVacioCorreo.style.display='block';
+                break;
+            case 'input-mensaje':
+                spanVacioMensaje.style.display='block';
+                break;
+            default:
+                break;
+        }
+    }
+
+    return campoVacio;
+}
+
+function comprobarCaracteresCorrectos(idCampo) {
+
+    if(idCampo === 'input-telefono') {
+
+        return comprobarTelefonoCorrecto(idCampo);
+
+    } else if( idCampo === 'input-correo') {
+
+        return comprobarCorreoCorrecto(idCampo);
+    }
+}
+
+function comprobarTelefonoCorrecto(idCampo) {
+
+    const campo = document.getElementById(idCampo);
+
+    const spanCaracteresIncorrectosTelefono = document.getElementById('error-caracteres-input-telefono');
+
+    const esTelefonoValido = /^\d{9}$/;
+
+    if(esTelefonoValido.test(campo.value)) {
+
+        campo.style.borderColor='#dee2e6';
+
+        spanCaracteresIncorrectosTelefono.style.display='none';
+
+        return true;
+
+    } else { //el telefono NO es valido
+
+        campo.style.borderColor='red';
+
+        spanCaracteresIncorrectosTelefono.style.display='block';
+
+        return false;
+
+    }
+
+}
+
+function comprobarCorreoCorrecto(idCampo) {
+
+    const campo = document.getElementById(idCampo);
+
+    const spanCaracteresIncorrectosCorreo = document.getElementById('error-caracteres-input-correo');
+
+    const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
+    if (correoRegex.test(campo.value)) { //si el texto SI tiene forma de correo
+
+        campo.style.borderColor='#dee2e6';
+
+        spanCaracteresIncorrectosCorreo.style.display = 'none';
+
+        return true;
+
+    } else { //si el texto NO tiene forma de correo
+
+        campo.style.borderColor='red';
+
+        spanCaracteresIncorrectosCorreo.style.display = 'block';
+
+        return false;
+    }
 }
